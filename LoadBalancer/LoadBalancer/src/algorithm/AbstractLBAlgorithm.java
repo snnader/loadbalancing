414415da93c4e5e43f61df1f91da0f078ff70f52
zzarc <sjqskqqp@gmail.com>
2022-10-20 16:01:36 -0700

    add exceptions for unimplemented functions

--- LoadBalancer/LoadBalancer/src/algorithm/AbstractLBAlgorithm.java
+++ LoadBalancer/LoadBalancer/src/algorithm/AbstractLBAlgorithm.java
@@ -8,15 +8,17 @@ public class AbstractLBAlgorithm implements LBAlgorithm {
 
     @Override
     public Worker choose() {
-        throw new UnsupportedOperationException("Must implement choose()");
+        throw new UnsupportedOperationException("Must implement choose() manually");
     }
 
     @Override
     public void onRequest(Request request) {
+        throw new UnsupportedOperationException("Must implement onRequest() manually");
     }
 
     @Override
     public void onResponse(Response response) {
+        throw new UnsupportedOperationException("Must implement onResponse() manually");
     }
 
     @Override
