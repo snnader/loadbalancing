abef623b8c667846ade9ebeaa1ec653b41a714e7
lanran12 <78628589+lanran12@users.noreply.github.com>
2022-10-25 19:29:26 -0700

    Add files via upload

---
+++ LoadBalancer/LoadBalancer/src/algorithm/LeastRecentlyUsed_LeastConnection.java
@@ -0,0 +1,93 @@
+package algorithm;
+
+import network.Request;
+import network.Response;
+import network.Worker;
+
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LeastRecentlyUsed_LeastConnection extends AbstractLBAlgorithm {
+    private List<Worker> workers;
+    private List<Worker> temp1;
+    private List<Worker> temp2;
+    private List<Integer> temp3;
+
+    private HashMap<Worker, Integer> collection;
+    private HashMap<Worker, Integer> timeNotUsed;
+
+    private Worker chooseWorker;
+    private int longestNotUsed;
+    private Lock lock;
+
+    private Random random = new Random();
+
+    public LeastRecentlyUsed_LeastConnection(List<Worker> workers) {
+        this.workers = workers;
+        longestNotUsed = 0;
+        collection = new HashMap<>();
+        timeNotUsed = new HashMap<>();
+        lock = new ReentrantLock();
+        for (Worker worker : workers) {
+            collection.put(worker, 0);
+            timeNotUsed.put(worker, 0);
+        }
+        
+    }
+    @Override
+    public Worker choose(Request request){
+        lock.lock();
+        temp1 = new ArrayList<>();
+        temp2 = new ArrayList<>();
+        temp3 = new ArrayList<>();
+
+        int minimumConnection = Collections.min(collection.values());
+        for (Worker worker : workers) {
+            if(collection.get(worker) == minimumConnection){
+                temp1.add(worker);
+            }
+            
+        }
+        System.out.println("temp1 size"+temp1.size());
+        for (Worker worker : workers) {
+            System.out.println(worker.ip + ":" + collection.get(worker));
+            System.out.println(timeNotUsed.get(worker));
+        }
+        if(temp1.size() > 1){
+            for (Worker worker : temp1) {
+                temp3.add(timeNotUsed.get(worker));
+            }
+            longestNotUsed = Collections.max(temp3);
+            for (Worker worker : temp1) {
+                if(timeNotUsed.get(worker) == longestNotUsed){
+                    temp2.add(worker);
+                }
+            }
+            System.out.println("temp2:"+temp2);
+            chooseWorker = temp2.get(random.nextInt(temp2.size()));
+        }else{
+            chooseWorker = temp1.get(0);
+        }
+        for (Worker worker : workers) {
+            if (worker != chooseWorker) {
+                timeNotUsed.put(worker, timeNotUsed.get(worker) + 1);
+            } else {
+                timeNotUsed.put(worker, 0);
+            }
+        }
+        lock.unlock();
+        return chooseWorker;
+    }
+
+
+    @Override
+    public void onRequest(Request request){
+        collection.put(request.worker, collection.get(request.worker) + 1);
+    }
+
+    @Override
+    public void onResponse(Response response){
+        collection.put(response.worker, collection.get(response.worker) - 1);
+    }
+}
