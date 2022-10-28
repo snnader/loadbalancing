6aca6ff1ba6ca58b0d7c6716a60a01d1c608970a
Salim Nader <61476414+snnader@users.noreply.github.com>
2022-10-28 00:57:31 -0700

    Add capacity field

--- LoadBalancer/LoadBalancer/src/network/Worker.java
+++ LoadBalancer/LoadBalancer/src/network/Worker.java
@@ -3,9 +3,16 @@ package network;
 public class Worker {
     public String ip;
     public String port;
+    public double capacity = 4.8;
 
     public Worker(String ip, String port) {
         this.ip = ip;
         this.port = port;
+    }
+
+    public Worker(String ip, String port, double capacity) {
+        this.ip = ip;
+        this.port = port;
+        this.capacity = capacity;
     }
 }
