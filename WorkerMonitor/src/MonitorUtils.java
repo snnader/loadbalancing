97d1423b88bb099be4dcdd14a8e656d295f70e12
zzarc <sjqskqqp@gmail.com>
2022-10-30 02:00:45 -0700

    fix inaccurate free memory info

--- WorkerMonitor/src/MonitorUtils.java
+++ WorkerMonitor/src/MonitorUtils.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
 public class MonitorUtils {
@@ -9,8 +12,24 @@ public class MonitorUtils {
     }
 
     public static double GetMemUsage() {
-        double total = bean.getTotalPhysicalMemorySize();
-        double free = bean.getFreePhysicalMemorySize();
-        return ((total - free) / total);
+
+        String fileName = "/proc/meminfo";
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Double total = 1.0 * Integer.valueOf(bufferedReader.readLine().split("\\s+")[1]) / (1024 * 1024);
+            bufferedReader.readLine();
+            Double avail = 1.0 * Integer.valueOf(bufferedReader.readLine().split("\\s+")[1]) / (1024 * 1024);
+
+            System.out.println("total: " + total + " avail: " + avail);
+
+            bufferedReader.close();
+            return ((total - avail) / total);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return 1.0;
     }
 }
