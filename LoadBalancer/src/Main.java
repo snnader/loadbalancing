450f93e7dc332246ac258e7965b442bfecc23955
Salim Nader <61476414+snnader@users.noreply.github.com>
2022-11-14 23:07:59 -0800

    Revert switch syntax and add terminated server IPs

--- LoadBalancer/src/Main.java
+++ LoadBalancer/src/Main.java
@@ -16,16 +16,16 @@ public class Main {
 
         // All worker nodes
         List<Worker> workers = new ArrayList<>(10);
-        workers.add(new Worker("172.31.17.150", "8001"));
-        workers.add(new Worker("172.31.18.180", "8001"));
-        workers.add(new Worker("172.31.14.254", "8001"));
-        workers.add(new Worker("172.31.5.174", "8001"));
-        workers.add(new Worker("172.31.15.101", "8001"));
-        workers.add(new Worker("172.31.12.170", "8001"));
-        workers.add(new Worker("172.31.0.255", "8001"));
-        workers.add(new Worker("172.31.6.95", "8001"));
-        workers.add(new Worker("172.31.11.132", "8001"));
-        workers.add(new Worker("172.31.2.68", "8001"));
+        workers.add(new Worker("172.31.17.150", "8001", 4.8));
+        workers.add(new Worker("172.31.18.180", "8001", 4.8));
+        workers.add(new Worker("172.31.14.254", "8001", 4.8));
+        workers.add(new Worker("172.31.5.174", "8001", 4.8));
+        workers.add(new Worker("172.31.15.101", "8001", 4.8));
+        workers.add(new Worker("172.31.12.170", "8001", 4.8));
+        workers.add(new Worker("172.31.0.255", "8001", 4.8*2));
+        workers.add(new Worker("172.31.6.95", "8001", 4.8*2));
+        workers.add(new Worker("172.31.11.132", "8001", 4.8*2));
+        workers.add(new Worker("172.31.2.68", "8001", 4.8*2));
 
         // Load Balancing Algorithm
         Scanner scanner = new Scanner(System.in);
@@ -34,18 +34,31 @@ public class Main {
         LBAlgorithm algorithm = null;
         while (algorithm == null) {
             switch (algoName) {
-                case "least usage", "1" -> algorithm = new LeastUsageFirst(workers);
-                case "least connection", "2" -> algorithm = new LeastRecentlyUsed_LeastConnection(workers);
-                case "response time", "3" -> algorithm = new LeastAvgResponseTimeAlgo(workers);
-                case "consistent hashing", "4" -> algorithm = new ConsistentHashAlgorithm(workers);
-                case "finish time", "5" -> algorithm = new EstimatedFinishTime(workers);
-                case "round robin", "6" -> algorithm = new RoundRobinAlgorithm(workers);
-                case "random", "7" -> algorithm = new RandomAlgorithm(workers);
-                default -> {
+                case "least usage": case "1":
+                    algorithm = new LeastUsageFirst(workers);
+                    break;
+                case "least connection": case "2":
+                    algorithm = new LeastRecentlyUsed_LeastConnection(workers);
+                    break;
+                case "response time": case "3":
+                    algorithm = new LeastAvgResponseTimeAlgo(workers);
+                    break;
+                case "consistent hashing": case "4":
+                    algorithm = new ConsistentHashAlgorithm(workers);
+                    break;
+                case "finish time": case "5":
+                    algorithm = new EstimatedFinishTime(workers);
+                    break;
+                case "round robin": case "6":
+                    algorithm = new RoundRobinAlgorithm(workers);
+                    break;
+                case "random": case "7":
+                    algorithm = new RandomAlgorithm(workers);
+                    break;
+                default:
                     System.out.println(" 1 least usage\n 2 least connection\n 3 response time\n 4 consistent hashing\n 5 finish time\n 6 round robin\n 7 random\n");
                     System.out.println("LB algorithm: ");
                     algoName = scanner.nextLine();
-                }
             }
         }
 
