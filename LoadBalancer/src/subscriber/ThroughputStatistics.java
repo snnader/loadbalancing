a27e5e1713a825133507a3a79441497ec8bc168f
zzarc <sjqskqqp@gmail.com>
2022-11-16 20:40:01 -0800

    make throughput log start after first request, add avg

--- LoadBalancer/src/subscriber/ThroughputStatistics.java
+++ LoadBalancer/src/subscriber/ThroughputStatistics.java
@@ -10,37 +10,48 @@ import java.util.concurrent.TimeUnit;
 
 public class ThroughputStatistics extends AbstractSubscriber {
     public int traffic = 0;
+    public int totalTraffic = 0;
     public long startTime = 0; // last start of a time period
     public long beginTime = 0; // log starting time
+    private boolean start = false; // only log after receive first request
 
     public ThroughputStatistics() {
-        Logger.log("Throughput", "[0] Started");
-        this.startTime = System.currentTimeMillis();
-        this.beginTime = System.currentTimeMillis();
-
         ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
         exec.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
 //                System.out.println("traffic: " + totalTraffic + " start: " + startTime + " current: " + System.currentTimeMillis() + " throughput: " + (String.valueOf(1000.0 * totalTraffic / (System.currentTimeMillis() - startTime))));
-                long currentTime = System.currentTimeMillis();
-                String info = "[" + ((currentTime-beginTime)/1000) + "] " + (String.valueOf(1000.0 * traffic / (currentTime - startTime)) + " B/s");
-                Logger.log("Throughput", info);
-
-                // Reset time and traffic
-                traffic = 0;
-                startTime = currentTime;
+                if (start) {
+                    long currentTime = System.currentTimeMillis();
+                    double currentThroughput = 1000.0 * traffic / (currentTime - startTime);
+                    String info = "[" + ((currentTime - beginTime) / 1000) + "] "
+                            + (String.valueOf(currentThroughput)) + " B/s"
+                            + " avg " + String.valueOf(1000.0 * totalTraffic / (currentTime - beginTime));
+                    System.out.println(info);
+                    Logger.log("Throughput", info);
+                    // Reset time and traffic
+                    traffic = 0;
+                    startTime = currentTime;
+                }
             }
         }, 0, 1, TimeUnit.SECONDS);
     }
 
     public synchronized void onRequest(Request request) {
         try {
+            if (!this.start) {
+                // start log from first request
+                this.start = true;
+                this.startTime = System.currentTimeMillis();
+                this.beginTime = System.currentTimeMillis();
+            }
             if (request.request.getRequestURI().getRawQuery() != null) {
                 traffic += request.request.getRequestURI().getRawQuery().getBytes().length;
+                totalTraffic += request.request.getRequestURI().getRawQuery().getBytes().length;
             }
             if (request.request.getRequestBody() != null) {
                 traffic += request.request.getRequestBody().toString().getBytes().length;
+                totalTraffic += request.request.getRequestBody().toString().getBytes().length;
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -50,9 +61,11 @@ public class ThroughputStatistics extends AbstractSubscriber {
     public synchronized void onResponse(Response response) {
         if (response.response.uri() != null) {
             traffic += response.response.uri().toString().getBytes().length;
+            totalTraffic += response.response.uri().toString().getBytes().length;
         }
         if (response.response.body() != null) {
             traffic += response.response.body().toString().getBytes().length;
+            totalTraffic += response.response.body().toString().getBytes().length;
         }
     }
 }
