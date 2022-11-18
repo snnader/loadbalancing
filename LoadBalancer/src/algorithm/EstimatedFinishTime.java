cd78d4e5b5020a2f3ff756c7fa9d122936f68542
Salim Nader <61476414+snnader@users.noreply.github.com>
2022-11-17 18:24:53 -0800

    Add onResponse and onRequestFail

--- LoadBalancer/src/algorithm/EstimatedFinishTime.java
+++ LoadBalancer/src/algorithm/EstimatedFinishTime.java
@@ -5,50 +5,57 @@ import network.Response;
 import network.Worker;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.function.UnaryOperator;
 
 public class EstimatedFinishTime extends AbstractLBAlgorithm {
 
-    private String name = "EstimatedFinishTime";
+    private final static String NAME = "EstimatedFinishTime";
 
     private final Worker[] workers;
-    private final double[] finishTime;
+    private final AtomicReferenceArray<Double> finishTime;
     private final double[] capacity;
+    private final ConcurrentHashMap<Worker, ConcurrentHashMap<String, Double>> requestMap;
 
     public EstimatedFinishTime(List<Worker> workers) {
         this.workers = new Worker[workers.size()];
-        this.finishTime = new double[workers.size()];
+        this.finishTime = new AtomicReferenceArray<>(workers.size());
         this.capacity = new double[workers.size()];
+        this.requestMap = new ConcurrentHashMap<>(workers.size());
         for (int i = 0; i < workers.size(); i++) {
             this.workers[i] = workers.get(i);
-            this.finishTime[i] = 0;
+            this.finishTime.set(i, 0.0d);
             this.capacity[i] = this.workers[i].capacity;
+            this.requestMap.put(this.workers[i], new ConcurrentHashMap<>());
         }
     }
 
     @Override
     public String getName() {
-        return this.name;
+        return NAME;
     }
 
     @Override
     public Worker choose(Request request) {
         int requestLoad = request.getLoad();
-        return this.workers[findMinFinishTime(requestLoad)];
+        return this.workers[findMinFinishTime(request.uuid, requestLoad)];
     }
 
-    private synchronized int findMinFinishTime(double requestLoad) {
+    private int findMinFinishTime(String uuid, double requestLoad) {
         // find worker with the least finish time
         int index = 0;
         double min = Double.MAX_VALUE;
         for (int i = 0; i < workers.length; i++) {
-            double estimatedFinishTime = this.finishTime[i] + requestLoad / this.capacity[i];
+            double estimatedFinishTime = this.finishTime.get(i) + requestLoad / this.capacity[i];
             if (estimatedFinishTime < min) {
                 index = i;
                 min = estimatedFinishTime;
             }
         }
         // update its finish time
-        this.finishTime[index] += requestLoad / this.capacity[index];
+        this.finishTime.set(index, min);
+        this.requestMap.get(this.workers[index]).put(uuid, requestLoad / this.capacity[index]);
         return index;
     }
 
@@ -59,12 +66,28 @@ public class EstimatedFinishTime extends AbstractLBAlgorithm {
 
     @Override
     public void onResponse(Response response) {
-
+        int index = -1;
+        for (int i = 0; i < this.workers.length; i++) {
+            if (this.workers[i] == response.worker) {
+                index = i;
+                break;
+            }
+        }
+        Double delta = this.requestMap.get(response.worker).remove(response.uuid);
+        this.finishTime.getAndUpdate(index, (value) -> value - delta );
     }
 
     @Override
     public void onRequestFail(Request request) {
-
+        int index = -1;
+        for (int i = 0; i < this.workers.length; i++) {
+            if (this.workers[i] == request.worker) {
+                index = i;
+                break;
+            }
+        }
+        Double delta = this.requestMap.get(request.worker).remove(request.uuid);
+        this.finishTime.getAndUpdate(index, (value) -> value - delta );
     }
 
 }
