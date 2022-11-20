1533b1f9cb401c7d4b38aabfd6233438ddb27b41
chengyu <zhang.chengyu1@northeastern.edu>
2022-11-19 23:09:24 -0800

    Update LARTF

--- LoadBalancer/src/algorithm/LeastAvgResponseTimeAlgo.java
+++ LoadBalancer/src/algorithm/LeastAvgResponseTimeAlgo.java
@@ -5,9 +5,12 @@ import network.Response;
 import network.Worker;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class LeastAvgResponseTimeAlgo extends AbstractLBAlgorithm {
 
@@ -15,27 +18,25 @@ public class LeastAvgResponseTimeAlgo extends AbstractLBAlgorithm {
 
     class Timestamp {
         public long requestTimestamp;
-        public long responseTimestamp;
-        public Timestamp(long requestTimestamp, long responseTimestamp) {
+        public long responseTimestamp = 0L;
+        public long responseTime = 0L;
+        public Timestamp(long requestTimestamp) {
             this.requestTimestamp = requestTimestamp;
-            this.responseTimestamp = responseTimestamp;
         }
-    }
 
-    class ResponseTime {
-        public long requestTimestampSum = 0;
-        public long responseTimestampSum = 0;
-        public int unrespondedCnt = 0;
+        public void setResponseTimestamp(long responseTimestamp) {
+            this.responseTimestamp = responseTimestamp;
+            this.responseTime = responseTimestamp - requestTimestamp;
+        }
 
-        public long calculate() {
-            return responseTimestampSum + unrespondedCnt * System.currentTimeMillis() - requestTimestampSum;
+        public long getTime() {
+            return responseTime > 0 ? responseTime : System.currentTimeMillis() - requestTimestamp;
         }
     }
 
     private List<Worker> workers;
     private HashMap<String, Timestamp> requestTimestampMap;
     private HashMap<Worker, Deque<String>> workerRequestMap;
-    private HashMap<Worker, ResponseTime> workerResponseTimeMap;
     private Integer windowSize;
     private Integer count;
     private boolean coldstart;
@@ -43,16 +44,14 @@ public class LeastAvgResponseTimeAlgo extends AbstractLBAlgorithm {
 
     public LeastAvgResponseTimeAlgo(List<Worker> workers) {
         this.workers = workers;
-        windowSize = 50;
+        windowSize = 20;
         count = 0;
         coldstart = true;
         requestTimestampMap = new HashMap<>();
         workerRequestMap = new HashMap<>();
-        workerResponseTimeMap = new HashMap<>();
         workerTotalMap = new HashMap<>();
         workers.forEach(t -> {
             workerRequestMap.put(t, new ArrayDeque<>());
-            workerResponseTimeMap.put(t, new ResponseTime());
             workerTotalMap.put(t, 0);
         });
     }
@@ -67,75 +66,74 @@ public class LeastAvgResponseTimeAlgo extends AbstractLBAlgorithm {
         if (coldstart) {
             Worker worker = workers.get(count % workers.size());
             count += 1;
-            if (count >= windowSize * workers.size() * this.workers.size() * 2) {
+            if (count >= windowSize * workers.size() * 1) {
                 coldstart = false;
             }
             workerTotalMap.put(worker, workerTotalMap.get(worker)+1);
             return worker;
         }
-        // compute avg time for each worker
-        Worker lowestWorker = workers.get(0);
-        double lowestResponseTime = !workerRequestMap.get(lowestWorker).isEmpty()?
-                1.0 * workerResponseTimeMap.get(lowestWorker).calculate() / workerRequestMap.get(lowestWorker).size()
-                : 0;
+  
+        List<Double> weights = new ArrayList<>();
+        Double weightSum = 0.0;
 
         for (Worker w : workers) {
-//            double responseTime = !workerRequestMap.get(w).isEmpty()?
-//                    1.0 * workerResponseTimeMap.get(w).calculate() / workerRequestMap.get(w).size()
-//                    : 0;
-
-            double responseTime = 1.0 * workerResponseTimeMap.get(w).calculate() / workerRequestMap.get(w).size();
-            System.out.println("Server " + w.ip + " " + w.port + ": " + workerResponseTimeMap.get(w).calculate() + " " + workerRequestMap.get(w).size());
+            double responseTime = workerRequestMap.get(w).stream().map(id -> requestTimestampMap.get(id).getTime()).reduce(0L, Long::sum);
+            weights.add(1/responseTime);
+            weightSum += 1/responseTime;
             System.out.println("Server " + w.ip + " " + w.port + ": " + responseTime + " total: " + workerTotalMap.get(w));
-            if (responseTime < lowestResponseTime) {
-                lowestWorker = w;
-                lowestResponseTime = responseTime;
+        }
+        
+        for (int i = 1; i < workers.size(); i++) {
+            weights.set(i, weights.get(i) + weights.get(i-1));
+        }
+        double weightSumFinal = weightSum;
+        weights = weights.stream().map(w -> w / weightSumFinal).collect(Collectors.toList());
+        Random rand = new Random();
+        double val = rand.nextDouble();
+        int left = 0;
+        int right = workers.size();
+        while (right > left) {
+            int mid = left + (right - left) / 2;
+            if (val > weights.get(mid)) {
+                left = mid+1;
+            } else {
+                right = mid;
             }
         }
-        workerTotalMap.put(lowestWorker, workerTotalMap.get(lowestWorker)+1);
-        return lowestWorker;
+        workerTotalMap.put(workers.get(left), workerTotalMap.get(workers.get(left))+1);
+        return workers.get(left);
     }
 
     @Override
     public synchronized void onRequest(Request request) {
         // if the number of requests surpass the window size
         Worker worker = request.worker;
-        requestTimestampMap.put(request.uuid, new Timestamp(System.currentTimeMillis(), 0));
+        long currentTime = System.currentTimeMillis();
+        requestTimestampMap.put(request.uuid, new Timestamp(currentTime));
         Deque<String> queue = workerRequestMap.get(worker);
 
         if (queue.size() == windowSize) {
-            String firstuuid = queue.getFirst();
-            ResponseTime responseTime = workerResponseTimeMap.get(worker);
-            responseTime.requestTimestampSum -= requestTimestampMap.get(firstuuid).requestTimestamp;
-            responseTime.responseTimestampSum -= requestTimestampMap.get(firstuuid).responseTimestamp;
-
-            if(requestTimestampMap.get(firstuuid).requestTimestamp == 0) {
-                responseTime.unrespondedCnt -= 1;
-            }
-
-            queue.removeFirst();
+            String firstuuid = queue.pop();
             requestTimestampMap.remove(firstuuid);
         }
 
         queue.addLast(request.uuid);
-        workerResponseTimeMap.get(worker).requestTimestampSum += requestTimestampMap.get(request.uuid).requestTimestamp;
-        workerResponseTimeMap.get(worker).unrespondedCnt += 1;
     }
 
     @Override
     public synchronized void onResponse(Response response) {
-        requestTimestampMap.get(response.uuid).responseTimestamp = System.currentTimeMillis();
-        ResponseTime responseTime = workerResponseTimeMap.get(response.worker);
-        responseTime.responseTimestampSum += System.currentTimeMillis();
-        responseTime.unrespondedCnt -= 1;
+        long currentTime = System.currentTimeMillis();
+        requestTimestampMap.get(response.uuid).setResponseTimestamp(currentTime);
     }
 
     @Override
     public synchronized void onRequestFail(Request request) {
-//        requestTimestampMap.get(request.uuid).responseTimestamp = System.currentTimeMillis();
-//        responseTime.get(request.worker) -= requestTimestampMap.get(request.uuid).requestTimestamp;
-//        ResponseTime responseTime = workerResponseTimeMap.get(request.worker);
-//        responseTime.responseTimestampSum += System.currentTimeMillis();
-//        responseTime.unrespondedCnt -= 1;
+        System.out.println("failed " + request.worker);
+        Worker worker = request.worker;
+        Deque<String> queue = workerRequestMap.get(worker);
+        if (queue.contains(request.uuid)){
+            queue.remove(request.uuid);
+            requestTimestampMap.remove(request.uuid);
+        }
     }
 }
